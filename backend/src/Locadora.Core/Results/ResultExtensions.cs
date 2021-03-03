127c0d31310bc07bf63d1e82dcd970908f59a2c5
jorgesebastiao <jorgenes1@hotmail.com>
2021-03-03 19:27:46 -0300

    Adicionado Results ao projeto core

---
+++ backend/src/Locadora.Core/Results/ResultExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Locadora.Core.Results
+{
+    // Baseado em: https://github.com/hugoestevam/OptimusPrime/tree/master/Server/robot.Domain/Results
+
+    using static Helpers;
+
+    public static class Result
+    {
+        public static Result<Exception, TSuccess> Run<TSuccess>(this Func<TSuccess> func)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
+
+        public static Result<Exception, Unit> Run(this Action action) => Run(ToFunc(action));
+        public static Result<Exception, TSuccess> Run<TSuccess>(this Exception ex) => ex;
+    }
+}
